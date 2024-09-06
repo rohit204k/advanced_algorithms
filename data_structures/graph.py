53022768741ba62bc870e406280d539bf5ece53c
rohit204k <gundakallirohit@gmail.com>
2024-09-05 23:27:47 -0400

    Adding karger's min cut algorithm code.

--- data_structures/graph.py
+++ data_structures/graph.py
@@ -1,12 +1,13 @@
 from collections import deque, defaultdict
 
-# Graph Class
-# vertices: list of vertices (vertices are numbered from 1 and onwards.)
-# edges: list of edges (each edge is a tuple of form (s,v), ex: (1,2))
-# weights: list of weights in order pass in edges, defaults to None, which is 1 for all edges
-# directed: True for directed graphs, False otherwise.
-
 class Graph:
+    '''
+    Graph Class
+    vertices: list of vertices (vertices are numbered from 1 and onwards.)
+    edges: list of edges (each edge is a tuple of form (s,v), ex: (1,2))
+    weights: list of weights in order pass in edges, defaults to None, which is 1 for all edges
+    directed: True for directed graphs, False otherwise.
+    '''
     def __init__(self, vertices, edges, weights=None, directed=False) -> None:
         self.v = vertices
         self.e = {}
@@ -85,5 +86,48 @@ class BipartiteGraph:
 
         for i, row in enumerate(self.adjacency_matrix):
             matrix_str += str(self.U[i]) + " " + " ".join(map(str, row)) + "\n"
+
+        return matrix_str
+
+
+class MultiGraph:
+    '''
+    The MultiGraph class is implemented specifically to solve the Karger's min cut problem. It is undirected unweighted graph with multiple edges
+    between any pair of vertices.
+    '''
+    def __init__(self, vertices, edges, weights=None, directed=False) -> None:
+        self.v = vertices
+        self.e = {}
+
+        # if weights is None:
+        #     weights = [1] * len(edges)
+
+        for i in range(len(edges)):
+            # self.e[edges[i]] += weights[i]
+            self.e[edges[i]] = self.e.get(edges[i], 0) + 1
+
+        self.adjacency_matrix = [[0] * len(self.v) for _ in range(len(self.v))]
+
+        for (u, v), weight in self.e.items():
+            self.adjacency_matrix[u - 1][v - 1] = weight
+            if not directed:
+                self.adjacency_matrix[v - 1][u - 1] = weight
+
+    def format_matrix_row(self, array, width = 3):
+        return [
+            f"{'inf' if val == float('inf') else int(val):>{width}}" for val in array
+        ]
+
+    def __str__(self):
+
+        matrix_str = "  " + " ".join(map(str, self.format_matrix_row(self.v))) + "\n"
+
+        for i in range(len(self.v)):
+            matrix_str += (
+                str(self.v[i])
+                + " "
+                + " ".join(map(str, self.format_matrix_row(self.adjacency_matrix[i])))
+                + "\n"
+            )
 
         return matrix_str
